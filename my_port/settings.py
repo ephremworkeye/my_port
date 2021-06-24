061071e9075b5b2ba26a14a47f442735d0df3f20
Ephrem Wube <ephremworkeye@gmail.com>
2021-06-24 13:46:12 -0700

    Structure added

--- my_port/settings.py
+++ my_port/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'port.apps.PortConfig', # added app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
